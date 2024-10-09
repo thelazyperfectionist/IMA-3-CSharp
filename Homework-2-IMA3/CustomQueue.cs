6b4889222d36a15c9079d945d09e526ae81bc031
Ani Tadevosyan <150780972+thelazyperfectionist@users.noreply.github.com>
2024-10-09 22:59:09 +0400

    Add files via upload

---
+++ Homework-2-IMA3/CustomQueue.cs
@@ -0,0 +1,101 @@
+﻿/* using System;
+
+class CustomQueue
+{
+    int front, rear, capacity;
+    int[] queue;
+
+    CustomQueue(int c)
+    {
+        front = 0;
+        rear = -1;
+        capacity = c;
+        queue = new int[capacity];
+    }
+
+    void Enqueue(int data)
+    {
+        if (rear == capacity - 1)
+        {
+            Console.WriteLine("Queue is full");
+            return;
+        }
+
+        queue[++rear] = data;
+    }
+
+    void Dequeue()
+    {
+        if (front > rear)
+        {
+            Console.WriteLine("Queue is empty");
+            return;
+        }
+
+        for (int i = 0; i < rear; i++)
+        {
+            queue[i] = queue[i + 1];
+        }
+
+        rear--;
+    }
+
+    void Display()
+    {
+        if (front > rear)
+        {
+            Console.WriteLine("Queue is Empty");
+            return;
+        }
+
+        for (int i = front; i <= rear; i++)
+        {
+            Console.Write(queue[i] + " <-- ");
+        }
+        Console.WriteLine();
+    }
+
+    void Front()
+    {
+        if (rear == -1)
+        {
+            Console.WriteLine("Queue is Empty");
+            return;
+        }
+        Console.WriteLine("Front Element is: " + queue[front]);
+    }
+
+    public static void Main(string[] args)
+    {
+        CustomQueue q = new CustomQueue(4);
+
+        q.Display();
+
+        q.Enqueue(20);
+        q.Enqueue(30);
+        q.Enqueue(40);
+        q.Enqueue(50);
+
+        q.Display();
+
+        q.Enqueue(60);
+
+        q.Display();
+
+        q.Dequeue();
+        q.Dequeue();
+
+        Console.WriteLine("After two node deletions");
+
+        q.Display();
+
+        Console.WriteLine("After one insertion");
+        q.Enqueue(60);
+
+        q.Display();
+
+        q.Front();
+    }
+}
+
+//To be honest, here i used GreekForgreek as a "assistant" */
---
+++ Homework-2-IMA3/Insertion.cs
@@ -0,0 +1,32 @@
+﻿using System; 
+  
+class Insertion
+{
+    //1
+    static void insertionSortRecursive(int[] arr,
+int n)
+    {
+        if (n <= 1)
+            return;
+        insertionSortRecursive(arr, n - 1);
+
+        int last = arr[n - 1];
+        int j = n - 2;
+
+        while (j >= 0 && arr[j] > last)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = last;
+    }
+    static void Main()
+    {
+        int[] arr = { 12, 11, 13, 5, 6 };
+
+        insertionSortRecursive(arr, arr.Length);
+
+        for (int i = 0; i < arr.Length; i++)
+            Console.Write(arr[i] + " ");
+    }
+}
